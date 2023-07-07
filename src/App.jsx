9128693040319dd6a9ec1540c5d283ce411765c1
Guilherme Lins <83785368+Glins-dev@users.noreply.github.com>
2023-07-07 19:24:53 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,42 @@
-import { useState } from 'react'
+import { useState } from 'react';
+
+import Todo from './components/Todo';
+
+import "./App.css";
+
+import TodoForm from './components/TodoForm';
 
 function App() {
+  const [todos,setTodos] = useState([
+    {
+      id: 1,
+      text:"criar funcinalidade X no sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Ir para a academia",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id:3,
+      text: "estudar React",
+      category: "Estudo",
+      isCompleted: false,
+    }
+  ])
 
-
-  return (
-    <div>
-      Olá React !!!
+  return <div className='app'>
+    <h1>Lista de Tarefas</h1>
+    <div className='todo-list'>
+      {todos.map((todo) => (
+        <Todo todo={todo}></Todo>
+      ))}
     </div>
-  )
+   <TodoForm></TodoForm>
+  </div>
 }
 
 export default App
