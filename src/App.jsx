3ad4b4f6ea7b1314a6b45569e3d8c662d38f83b9
Guilherme Lins <83785368+Glins-dev@users.noreply.github.com>
2023-07-10 18:48:55 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -26,17 +26,31 @@ function App() {
       category: "Estudo",
       isCompleted: false,
     }
-  ])
+  ]);
 
-  return <div className='app'>
+  const addTodo = (text, category) => {
+
+    const newTodos = [
+      ...todos, {
+        id: Math.floor(Math.random()*10000),
+        text,
+        category,
+        isCompleted: false,
+    },
+  ];
+    setTodos(newTodos);
+  }
+
+  return (<div className='app'>
     <h1>Lista de Tarefas</h1>
     <div className='todo-list'>
       {todos.map((todo) => (
-        <Todo todo={todo}></Todo>
+        <Todo key={todo.id} todo={todo}/>
       ))}
     </div>
-   <TodoForm></TodoForm>
+   <TodoForm addTodo={addTodo} />
   </div>
+  );
 }
 
 export default App
--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -9,8 +9,8 @@ const Todo = ({todo}) => {
       <p>({todo.category})</p>
     </div>
     <div>
-      <button>Completar</button>
-      <button>X</button>
+      <button className='complete'>Completar</button>
+      <button className='remove'>X</button>
     </div>
   </div>
   )
